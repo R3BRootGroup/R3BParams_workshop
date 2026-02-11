a52bab6ada45b981366170a2c098e821d3a1ba17
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2026-02-11 09:34:01 +0100

    Minor change in ana_music.C

--- macros/sim/ana_music.C
+++ macros/sim/ana_music.C
@@ -20,75 +20,74 @@
 #include <TSystem.h>
 #include <memory>
 
-void ana_music(const float nev = -1) 
+void ana_music(const float nev = -1)
 {
-  // Timer
-  TStopwatch timer;
-  timer.Start();
+    // Timer
+    TStopwatch timer;
+    timer.Start();
 
-  // INPUT FILE
-  TFile *file = TFile::Open("music.sim.root");
+    // INPUT FILE
+    TFile* file = TFile::Open("sim.root");
 
-  // READING TREE
-  TTree *tree = dynamic_cast<TTree *>(file->Get("evt"));
+    // READING TREE
+    TTree* tree = dynamic_cast<TTree*>(file->Get("evt"));
 
-  // TREE ENTRIES
-  Int_t nevents = tree->GetEntries();
-  if (nev > -1)
-    nevents = nev;
-  std::cout << "Events: " << nevents << std::endl;
+    // TREE ENTRIES
+    Int_t nevents = tree->GetEntries();
+    if (nev > -1)
+        nevents = nev;
+    std::cout << "Events: " << nevents << std::endl;
 
-  // MCTrack Data
-  auto MCTrackCA = new TClonesArray("R3BMCTrack");
-  TBranch *branchMCTrack = dynamic_cast<TBranch *>(tree->GetBranch("MCTrack"));
-  branchMCTrack->SetAddress(&MCTrackCA);
+    // MCTrack Data
+    auto MCTrackCA = new TClonesArray("R3BMCTrack");
+    auto* branchMCTrack = dynamic_cast<TBranch*>(tree->GetBranch("MCTrack"));
+    branchMCTrack->SetAddress(&MCTrackCA);
 
-  // MUSIC Point Data
-  auto musicpoint = new TClonesArray("R3BMusicPoint");
-  TBranch *branchMusicData =
-      dynamic_cast<TBranch *>(tree->GetBranch("MusicPoint"));
-  if (branchMusicData)
-    branchMusicData->SetAddress(&musicpoint);
+    // MUSIC Point Data
+    auto musicpoint = new TClonesArray("R3BMusicPoint");
+    auto* branchMusicData = dynamic_cast<TBranch*>(tree->GetBranch("MusicPoint"));
+    if (branchMusicData)
+        branchMusicData->SetAddress(&musicpoint);
 
-  // Histograms
-  auto h2_theta_ek =
-      new TH2F("h2_Theta_Ek", "Theta vs Ek", 1000, 0., 150., 1200, 0., 1200.);
+    // Histograms
+    auto h2_theta_ek = new TH2F("h2_Theta_Ek", "Theta vs Ek", 1000, 0., 150., 1200, 0., 1200.);
 
-  // LOOP IN THE EVENTS
-  for (int ie = 0; ie < nevents; ie++) {
-    // Print current event in the analysis
-    printf("Processed: %d of %d (%.2f of 100) \r", ie + 1, nevents,
-           100. * (ie + 1.0) / nevents);
-    fflush(stdout);
+    // LOOP IN THE EVENTS
+    for (int ie = 0; ie < nevents; ie++)
+    {
+        // Print current event in the analysis
+        printf("Processed: %d of %d (%.2f of 100) \r", ie + 1, nevents, 100. * (ie + 1.0) / nevents);
+        fflush(stdout);
 
-    // Clean the TClonesArrays
-    musicpoint->Clear();
-    MCTrackCA->Clear();
+        // Clean the TClonesArrays
+        musicpoint->Clear();
+        MCTrackCA->Clear();
 
-    // Fill the TClonesArrays
-    tree->GetEntry(ie);
+        // Fill the TClonesArrays
+        tree->GetEntry(ie);
 
-    // Read MCTrack Data
-    int MCtracksPerEvent = MCTrackCA->GetEntriesFast();
-    if (MCtracksPerEvent > 0) {
-      for (int j = 0; j < MCtracksPerEvent; j++) {
-        auto track = dynamic_cast<R3BMCTrack *>(MCTrackCA->At(j));
-        auto mom = track->GetMomentumMass() * 1000.; // MeV
-        auto ekin = (mom.E() - mom.M());
-        auto ekin_per_n = ekin / track->GetMass();
-        //  cout <<ekin_per_n << endl;
-        h2_theta_ek->Fill(mom.Theta() * TMath::RadToDeg(), ekin_per_n);
-      }
+        // Read MCTrack Data
+        int MCtracksPerEvent = MCTrackCA->GetEntriesFast();
+        if (MCtracksPerEvent > 0)
+        {
+            for (int j = 0; j < MCtracksPerEvent; j++)
+            {
+                auto track = dynamic_cast<R3BMCTrack*>(MCTrackCA->At(j));
+                auto mom = track->GetMomentumMass() * 1000.; // MeV
+                auto ekin = (mom.E() - mom.M());
+                auto ekin_per_n = ekin / track->GetMass();
+                //  cout <<ekin_per_n << endl;
+                h2_theta_ek->Fill(mom.Theta() * TMath::RadToDeg(), ekin_per_n);
+            }
+        }
     }
-  }
 
-  // Define TCanvas and draw histograms, ...
-  auto c1 = new TCanvas("c1");
-  h2_theta_ek->Draw("col");
+    // Define TCanvas and draw histograms, ...
+    auto c1 = new TCanvas("c1");
+    h2_theta_ek->Draw("col");
 
-  // Report
-  timer.Stop();
-  std::cout << "Real time: " << timer.RealTime()
-            << " s, CPU time: " << timer.CpuTime() << " s" << std::endl;
-  std::cout << "Macro finished successfully." << std::endl;
+    // Report
+    timer.Stop();
+    std::cout << "Real time: " << timer.RealTime() << " s, CPU time: " << timer.CpuTime() << " s" << std::endl;
+    std::cout << "Macro finished successfully." << std::endl;
 }
